8d76f0115efa67d33e77b3528ff35c71f5327896
Blackboule2000 <louis.boulenouar@gmail.com>
2025-05-16 14:19:40 +0200

    Correction des lecteurs média

---
+++ supabase/migrations/20250516121930_ivory_peak.sql
@@ -0,0 +1,35 @@
+UPDATE program
+SET 
+  video_url = 'https://www.youtube.com/embed/5akvnSXO4wQ',
+  audio_url = null
+WHERE title = 'MOTOLO';
+
+UPDATE program
+SET 
+  video_url = 'https://www.youtube.com/embed/-Jt6i24KZpA',
+  audio_url = null
+WHERE title = 'Arbas';
+
+UPDATE program
+SET 
+  audio_url = 'https://ia800300.us.archive.org/26/items/audio-2025-04-28-14-01-03-1/AUDIO-2025-04-28-14-01-03%281%29.m4a',
+  video_url = null
+WHERE title = 'Swinggums';
+
+UPDATE program
+SET 
+  audio_url = 'https://ia600706.us.archive.org/17/items/never-comes-heri-fara/Never%20Comes%20-%20Heri%20Fara.mp3',
+  video_url = null
+WHERE title = 'Herifara';
+
+UPDATE program
+SET 
+  video_url = 'https://www.youtube.com/embed/1EbzcOcWoog',
+  audio_url = null
+WHERE title = 'Philox';
+
+UPDATE program
+SET 
+  video_url = 'https://www.youtube.com/embed/vtFg8ciwfmE',
+  audio_url = null
+WHERE title = 'Anna Rudy & Paul Lazarus';

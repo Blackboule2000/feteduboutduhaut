0892ed19f2d01e9be7dd892c3ca546954a258eed
Blackboule2000 <louis.boulenouar@gmail.com>
2025-06-08 13:24:09 +0200

    Updated HeroForm.tsx

--- src/components/admin/HeroForm.tsx
+++ src/components/admin/HeroForm.tsx
@@ -24,7 +24,7 @@ const defaultSettings: HeroSettings = {
   subtitle: 'Festival Musical et Culturel',
   date: '26 Juillet 2025',
   main_image: 'http://www.image-heberg.fr/files/1747215665990305646.png',
-  poster_image: 'http://www.image-heberg.fr/files/17472124523185540990.jpg',
+  poster_image: 'http://www.image-heberg.fr/files/17493815783183277355.jpg',
   background_color: '#f6d9a0',
   social_links: {
     facebook: 'https://www.facebook.com/AssociationDuBoutDuHaut',
